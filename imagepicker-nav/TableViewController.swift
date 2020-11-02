66178c45ba5fa8831677349e75e59282c9848de1
Nasty <randomguyplaying@gmail.com>
2020-11-02 18:55:19 +0800

    Create TableViewController.swift

---
+++ imagepicker-nav/TableViewController.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var itemList:[Item] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return itemList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = itemList[indexPath.row].name
+        cell.imageView?.image = itemList[indexPath.row].picture
+
+        return cell
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            itemList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+    @IBAction func goBack (_ segue:UIStoryboardSegue) {
+        let controller = segue.source as! ViewController
+        
+        if controller.imgSelection.image != nil {
+            itemList.append(Item(itemName: controller.txtTitle.text!, itemPic: controller.imgSelection.image!))
+        }
+    }

256646ac1164ada8a4b9401b23dc9eaa1a804614
Nasty <randomguyplaying@gmail.com>
2020-11-02 18:50:03 +0800

    Create item.swift

---
+++ imagepicker-nav/item.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+
+class Item {
+  var name:String
+  var picture:UIImage
+  
+  init(itemName:String, itemPic:UIImage){
+    self.name = itemName
+    self.picture = itemPic
+  }
+}

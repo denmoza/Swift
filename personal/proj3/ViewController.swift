a2878669fc185347f8042e840199c68a21a41f96
Nasty <randomguyplaying@gmail.com>
2020-11-06 01:53:03 +0800

    Add files via upload

---
+++ personal/proj3/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  forfun7
+//
+//  Created by Denmoza on 5/11/20.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func showMiracle() {
+        let slideVC = OverlayView()
+        slideVC.modalPresentationStyle = .custom
+        slideVC.transitioningDelegate = self
+        self.present(slideVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func onButton(_ sender: Any) {
+        showMiracle()
+        print("Aaaaaaaaaaaaaaaaaa")
+    }
+    
+}
+
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        PresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
